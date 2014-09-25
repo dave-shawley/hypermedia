a5a18675b75dc5198ee84d0acff2cd0d0e891855
Dave Shawley <daveshawley@gmail.com>
2014-09-25 07:42:42 -0400

    tornado_linker_tests: Simplify, simplify, simplify.

--- tests/tornado_linker_tests.py
+++ tests/tornado_linker_tests.py
@@ -31,33 +31,56 @@ app.add_handlers(r'65\.199\.32\.155', [
 ])
 
 
-class RequestTestMixin(object):
-
-    def create_request(self, method, uri, headers=None):
-        # Setting X-Forwarded-For is necessary to make our tests
-        # work well with Tornado 3.x.  In 3.x, the remote_ip keyword
-        # to HTTPRequest
-        context = mock.Mock(remote_ip='172.16.0.1', protocol='http')
-        request_headers = {'X-Forwarded-For': '172.16.0.1'}
-        if headers is not None:
-            request_headers.update(headers)
-        return httpserver.HTTPRequest(
-            method=method,
-            uri=uri,
-            connection=mock.Mock(context=context),
-            headers=request_headers,
-        )
+def create_request(method, uri, headers=None):
+    # Setting X-Forwarded-For is necessary to make our tests
+    # work well with Tornado 3.x.  In 3.x, the remote_ip keyword
+    # to HTTPRequest
+    context = mock.Mock(remote_ip='172.16.0.1', protocol='http')
+    request_headers = {'X-Forwarded-For': '172.16.0.1'}
+    if headers is not None:
+        request_headers.update(headers)
+    return httpserver.HTTPRequest(
+        method=method,
+        uri=uri,
+        connection=mock.Mock(context=context),
+        headers=request_headers,
+    )
 
 
-class WhenAddingSimpleLink(RequestTestMixin, unittest.TestCase):
+class WhenCreatingLinker(unittest.TestCase):
+
+    def setUp(self):
+        super(WhenCreatingLinker, self).setUp()
+        self.linker = mixins.Linker()
+
+    def test_that_request_attribute_is_created(self):
+        self.assertIsNone(self.linker.request)
+
+    def test_that_application_attribute_is_created(self):
+        self.assertIsNone(self.linker.application)
+
+
+class WhenCreatingLinkerMixedInOverRequestHandler(unittest.TestCase):
+
+    def setUp(self):
+        super(WhenCreatingLinkerMixedInOverRequestHandler, self).setUp()
+        self.request = create_request('GET', '/')
+        self.linker = MovieHandler(app, self.request)
+
+    def test_that_request_is_preserved(self):
+        self.assertIs(self.linker.request, self.request)
+
+    def test_that_application_is_preserved(self):
+        self.assertIs(self.linker.application, app)
+
+
+class WhenAddingSimpleLink(unittest.TestCase):
 
     def setUp(self):
         super(WhenAddingSimpleLink, self).setUp()
         self.handler = MovieHandler(
-            app,
-            self.create_request('GET', '/movie/1',
-                                headers={'Host': '10.0.0.1:8080'}),
-        )
+            app, create_request('GET', '/movie/1',
+                                headers={'Host': '10.0.0.1:8080'}))
         self.handler.add_link('comments', CommentHandler, 'GET', uid=1)
 
     @property
@@ -72,15 +95,11 @@ class WhenAddingSimpleLink(RequestTestMixin, unittest.TestCase):
                          'http://10.0.0.1:8080/comments/1')
 
 
-class WhenAddingQueryLink(RequestTestMixin, unittest.TestCase):
+class WhenAddingQueryLink(unittest.TestCase):
 
     def setUp(self):
         super(WhenAddingQueryLink, self).setUp()
-        self.handler = MovieHandler(
-            app,
-            self.create_request('GET', '/movie/1',
-                                headers={'Host': '10.0.0.1:8080'}),
-        )
+        self.handler = MovieHandler(app, create_request('GET', '/movie/1'))
         self.handler.add_link('find-actors', SearchHandler, 'GET', query={
             'movie': '/movie/1', 'search-for': 'actors',
         })
@@ -100,51 +119,37 @@ class WhenAddingQueryLink(RequestTestMixin, unittest.TestCase):
         )
 
 
-class WhenAddingLinkToNonexistentHandler(RequestTestMixin, unittest.TestCase):
+class WhenAddingLinkToNonexistentHandler(unittest.TestCase):
 
     def setUp(self):
         super(WhenAddingLinkToNonexistentHandler, self).setUp()
-        self.handler = MovieHandler(
-            app,
-            self.create_request('GET', '/movie/1',
-                                headers={'Host': '10.0.0.1:8080'}),
-        )
+        self.handler = MovieHandler(app, create_request('GET', '/movie/1'))
 
     def test_that_internal_error_is_raised(self):
         with self.assertRaises(web.HTTPError):
             self.handler.add_link('whaeva', web.RequestHandler, 'GET')
 
 
-class WhenAddingLinkFromSpecificVirtualHost(
-        RequestTestMixin, unittest.TestCase):
+class WhenAddingLinkFromSpecificVirtualHost(unittest.TestCase):
 
     def setUp(self):
         super(WhenAddingLinkFromSpecificVirtualHost, self).setUp()
         self.handler = MovieHandler(
-            app,
-            self.create_request('GET', '/movie/1',
-                                headers={'Host': '65.199.32.155'})
-        )
+            app, create_request('GET', '/movie/1',
+                                headers={'Host': '65.199.32.155'}))
         self.handler.add_link('add-comment', CommentHandler, 'POST', uid=1)
 
     def test_that_generated_link_has_correct_host(self):
-        print('handlers', app.handlers)
-        print('get_link_map', self.handler.get_link_map())
         generated_link = compat.urlsplit(
             self.handler.get_link_map()['add-comment']['url'])
-        print('generated_link', generated_link)
         self.assertEqual(generated_link.netloc, '65.199.32.155')
 
 
-class WhenAddingLinkWithMissingParameter(RequestTestMixin, unittest.TestCase):
+class WhenAddingLinkWithMissingParameter(unittest.TestCase):
 
     def setUp(self):
         super(WhenAddingLinkWithMissingParameter, self).setUp()
-        self.handler = MovieHandler(
-            app,
-            self.create_request('GET', '/movie/1',
-                                headers={'Host': '10.0.0.1:8080'}),
-        )
+        self.handler = MovieHandler(app, create_request('GET', '/movie/1'))
 
     def test_that_internal_error_is_raised(self):
         with self.assertRaises(web.HTTPError):
